56b2115dd4bfe9afcd354a4ce43dc1a3b67f6f2a
brobadosdi <brobadosdi@gmail.com>
2026-03-12 23:31:23 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -78,7 +78,7 @@ async def scraper_hashtags(hashtags: list[str], videos_par_hashtag: int) -> list
             num_sessions=1,
             sleep_after=3,
             headless=True,
-            browser="chromium",
+            browser="webkit",
         )
 
         toutes_videos = []
@@ -112,7 +112,7 @@ async def scraper_commentaires(api_instance, videos: list[dict], nb_commentaires
             num_sessions=1,
             sleep_after=3,
             headless=True,
-            browser="chromium",
+            browser="webkit",
         )
 
         for video in videos:
